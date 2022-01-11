bda7715eed48ea2e272022f535ff845359d36b0c
FOUZIA HAJ <hfouzia@hotmail.fr>
2022-01-11 21:07:08 +0100

    ajout infos cards

---
+++ src/app/services/room.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { map, catchError, retry } from 'rxjs/operators';
+
+import { BASE_API } from '../constants/base-api';
+import { handleError } from '../constants/handle-http-errors';
+import { Room } from '../interfaces/room';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoomService {
+
+  constructor(private http: HttpClient) {}
+
+  getListRoom(): Observable<Room[]> {
+    return (
+      this.http
+        .get<Room[]>(`${BASE_API}/rooms/`)
+        .pipe(map((results) => {
+            retry(3),
+            catchError(handleError);
+            return results;
+          }))
+    );
+  }
+}
