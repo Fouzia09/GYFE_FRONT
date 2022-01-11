235b8444e84cf5d6a5007ccbdceb1d9a84c2332e
FOUZIA HAJ <hfouzia@hotmail.fr>
2022-01-11 21:10:38 +0100

    ajout infos cards

---
+++ src/app/interfaces/room.ts
@@ -0,0 +1,14 @@
+export interface Room {
+    id: number;
+    name: string,
+    descriptif: string,
+    country: string,
+    city: string,
+    price: number,
+    image1: string,
+    image2: string,
+    image3: string,
+    isKingSize: boolean,
+    nbBed: number,
+    squarFeet: number
+}
