100e5a2a9d61e18a67440d6381e28a915126bbfc
agoss97 <emeryagossou3@gmail.com>
2022-01-17 23:30:14 +0100

    page detail component

--- src/app/pages/restaurant/restaurant-routing.module.ts
+++ src/app/pages/restaurant/restaurant-routing.module.ts
@@ -1,8 +1,10 @@
+import { DetailRestaurantComponent } from './detail-restaurant/detail-restaurant.component';
 import { ListRestaurantComponent } from './list-restaurant/list-restaurant.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: 'detail/:id', component: DetailRestaurantComponent },
   {path: 'list-restaurant', component: ListRestaurantComponent}
 ];
 
